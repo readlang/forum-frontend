58abc6badf06b2d626dc223379ce94df4f88b826
Read Langworthy <rlangworthy@gmail.com>
2023-06-07 16:17:20 -0700

    installed daisyUI

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import Context from "@/components/Context"
 import { Header } from "@/components/Header"
-// import Script from "next/script";  // added to use Script
 import './globals.css';  // needed for tailwind
 
 export const metadata = {
@@ -13,8 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <head>
-        {/* <Script src="https://cdn.tailwindcss.com"></Script>  added to try out tailwind */}
-        <link href="https://cdn.jsdelivr.net/npm/daisyui@3.0.2/dist/full.css" rel="stylesheet" type="text/css" /> {/* added to try out daisy UI */}
+        
       </head>
       <body className="container mx-auto px-4 " >
         <Context>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [ "light", "dark", "business" ],
+  }
 }
 
